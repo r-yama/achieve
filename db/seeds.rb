dcf022a65377fc305d80dadf2083c32daaf1d215
Ryuta Yamashita <r.yamashita69@gmail.com>
2017-04-10 02:56:22 +0000

    Create association

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |n|
+  email = Faker::Internet.email
+  name = Faker::Name.name
+  password = "password"
+  User.create!(email: email,
+               name: name,
+               password: password,
+               password_confirmation: password,
+               )
+end
+
+n = 1
+while n <= 100
+  Blog.create(
+    title: "あああ",
+    user_id: n
+  )
+  n = n + 1
+end
