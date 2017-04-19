a31b3d7ea01d3e3d4ffcd2587ffb6cca8a5d352a
Ryuta Yamashita <r.yamashita69@gmail.com>
2017-04-19 01:44:11 +0000

    Update notice_mailer.rb

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -15,7 +15,7 @@ class NoticeMailer < ApplicationMailer
   def sendmail_contact(contact)
     @contact = contact
 
-    mail to: "自分のメールアドレス",
-         subject: '【Achieve】お問い合わせが投稿されました'
+    mail to: @contact.email,
+         subject: '【Achieve】お問い合わせを承りました'
   end
 end
